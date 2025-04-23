67b7c5b1f26fe4a2e4da882fac322248fe67ca51
Aliya420 <aliya.s83@mail.ru>
2025-04-23 15:15:47 +0600

    5hw

--- Lessons/lesson_5.py
+++ Lessons/lesson_5.py
@@ -1,21 +1,18 @@
-# import random
-#
-# print(random.randint(1,10))
 
 from random import randint as generate_number, choice
-import utils.calculator as calc
+from utils.calculator import *
 from utils.templates import Person
 from termcolor import cprint
 from decouple import config
-
 print(choice(["one", "two", "three"]))
 print(generate_number(5, 15))
-print(calc.multiplication(7, 2))
+print(multiplication(7, 2))
 
 my_friend = Person('Jane', 25)
 print(my_friend)
-cprint("Hello, World!", "green")
+cprint("Hello, World!", "yellow", "on_red")
 
-print(config('DATABASE_URL'))
-commented = config('COMMENTED', default=0, cast=int)
-print(commented * 2)
+#
+# print(config('DATABASE_URL'))
+# commented = config('COMMENTED', default=0, cast=int)
+# print(commented * 2)
